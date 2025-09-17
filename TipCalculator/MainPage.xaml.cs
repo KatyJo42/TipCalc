469ce8ca2f3494f217ce8095cea7c56a8292cf5c
Katy Johnston <300077526@fvtc.edu>
2025-09-17 17:23:51 -0500

    updates

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -10,8 +10,16 @@ public partial class MainPage : ContentPage
         Title = "Tip Calculator";
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private void Multiply_OnClicked(object sender, EventArgs e)
     {
-        
+        double dblFifteen, dblTwenty, dblTwentyFive;
+        Double.TryParse(txtFifteenPercent.Text, out dblFifteen);
+        Double.TryParse(txtTwentyPercent.Text, out dblTwenty);
+        Double.TryParse(txtTwentyFivePercent.Text, out dblTwentyFive);
+
+        lblDisplay.Text = dblFifteen("bill amount").ToString();
+        lblDisplay.Text = dblTwenty("bill amount").ToString();
     }
+
+  
 }
